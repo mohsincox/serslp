1f289ec7fec3126451f7b6d1b36eb82adf294360
mohsincox <mohsincse2015@gmail.com>
2022-11-01 16:37:26 +0600

    bt

--- controllers/commonController.js
+++ controllers/commonController.js
@@ -1,6 +1,11 @@
 const db = require("../models");
 const Country = db.country;
 const Tournament = db.tournament;
+const TournamentTeamPlayerDetail = db.tournamentTeamPlayerDetail;
+const Player = db.player;
+const Game = db.game;
+const Franchise = db.franchise;
+const TournamentTeam = db.tournamentTeam;
 
 const countryGetAllCommon = (req, res) => {
   Country.findAll()
@@ -26,8 +31,60 @@ const tournamentGetCommon = (req, res) => {
     });
 };
 
+const tournamentTeamPlayerDetailGetAllCommon = (req, res) => {
+  TournamentTeamPlayerDetail.findAll({
+    where: {
+      tournament_id: req.params.tourId,
+    },
+    include: [
+      {
+        model: Player,
+        include: [
+          {
+            model: Game,
+          },
+          {
+            model: Country,
+          },
+          {
+            model: Franchise,
+          },
+        ],
+      },
+    ],
+  })
+    .then((tournamentTeamPlayerDetail) =>
+      res.status(200).send(tournamentTeamPlayerDetail)
+    )
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+const tournamentTeamGetAllCommon = (req, res) => {
+  TournamentTeam.findAll({
+    where: {
+      tournament_id: req.params.tourId,
+    },
+    include: [
+      {
+        model: Country,
+      },
+      {
+        model: Franchise,
+      },
+    ],
+  })
+    .then((games) => res.status(200).send(games))
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
 module.exports = {
   countryGetAllCommon,
   countryGetCommon,
   tournamentGetCommon,
+  tournamentTeamPlayerDetailGetAllCommon,
+  tournamentTeamGetAllCommon,
 };
--- routes/commonRoutes.js
+++ routes/commonRoutes.js
@@ -4,11 +4,20 @@ const {
   countryGetAllCommon,
   countryGetCommon,
   tournamentGetCommon,
+  tournamentTeamPlayerDetailGetAllCommon,
+  tournamentTeamGetAllCommon,
 } = require("../controllers/commonController");
 
 router.get("/countries", countryGetAllCommon);
 router.get("/country/:id", countryGetCommon);
 
 router.get("/tournament/:id", tournamentGetCommon);
+
+router.get(
+  "/tournament-team-player-deatil/:tourId",
+  tournamentTeamPlayerDetailGetAllCommon
+);
+
+router.get("/tournament-team/:tourId", tournamentTeamGetAllCommon);
 
 module.exports = router;
