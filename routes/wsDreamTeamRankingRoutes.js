14145c162a3706f91364318a132d38a5ed109c76
mohsincox <mohsincse2015@gmail.com>
2022-11-01 12:59:54 +0600

    dtr

--- routes/wsDreamTeamRankingRoutes.js
+++ routes/wsDreamTeamRankingRoutes.js
@@ -121,4 +121,30 @@ router.get("/dtr/:id", (req, res) => {
     });
 });
 
+router.get("/dtr-all/:id", (req, res) => {
+  TeamDetail.findAll({
+    attributes: [
+      "user_id",
+      "team_id",
+      "tournament_id",
+      [sequelize.fn("sum", sequelize.col("total_point")), "total_point"],
+    ],
+    group: "team_id",
+    where: { tournament_id: req.params.id },
+    order: sequelize.literal("total_point DESC"),
+    include: [
+      {
+        model: User,
+      },
+      {
+        model: Tournament,
+      },
+    ],
+  })
+    .then((tournaments) => res.status(200).send(tournaments))
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
 module.exports = router;
--- routes/wsFixtureRoutes.js
+++ routes/wsFixtureRoutes.js
@@ -48,6 +48,49 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/active", (req, res) => {
+  Tournament.findAll({
+    where: {
+      status: "Active",
+    },
+    include: [
+      {
+        model: Match,
+        include: [
+          {
+            model: TournamentTeam,
+            as: "tournament_team_one",
+            include: [
+              {
+                model: Country,
+              },
+              {
+                model: Franchise,
+              },
+            ],
+          },
+          {
+            model: TournamentTeam,
+            as: "tournament_team_two",
+            include: [
+              {
+                model: Country,
+              },
+              {
+                model: Franchise,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  })
+    .then((tournaments) => res.status(200).send(tournaments))
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
 router.get("/tt/:id", (req, res) => {
   TournamentTeam.findAll({
     where: {
