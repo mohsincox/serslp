db1c49c775cd6f573a8f4726eb5df189d3dacee7
work-kundefine <work.kundefine@gmail.com>
2022-11-09 11:04:49 +0600

    working football_player_point

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -2,76 +2,76 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const {
-  matchAdd,
-  matchGetAll,
-  matchGet,
-  matchUpdate,
-  matchDelete,
-  activeMatchGetAll,
+    matchAdd,
+    matchGetAll,
+    matchGet,
+    matchUpdate,
+    matchDelete,
+    activeMatchGetAll,
 } = require("../controllers/matchController");
 
 require("../config/passport")(passport);
 
 router.post(
-  "/",
-  [
+    "/",
+    [
+        passport.authenticate("jwt", {
+            session: false,
+        }),
+        // matchDuplicateCheck,
+    ],
+    matchAdd
+);
+
+router.get(
+    "/",
     passport.authenticate("jwt", {
-      session: false,
+        session: false,
     }),
-    // matchDuplicateCheck,
-  ],
-  matchAdd
+    matchGetAll
 );
 
 router.get(
-  "/",
-  passport.authenticate("jwt", {
-    session: false,
-  }),
-  matchGetAll
+    "/active/",
+    passport.authenticate("jwt", {
+        session: false,
+    }),
+    activeMatchGetAll
 );
 
 router.get(
-  "/active/",
-  passport.authenticate("jwt", {
-    session: false,
-  }),
-  activeMatchGetAll
+    "/:id",
+    passport.authenticate("jwt", {
+        session: false,
+    }),
+    matchGet
 );
 
 router.get(
-  "/:id",
-  passport.authenticate("jwt", {
-    session: false,
-  }),
-  matchGet
-);
-
-router.get(
-  "/pt/:id",
-  passport.authenticate("jwt", {
-    session: false,
-  }),
-  matchGet
+    "/pt/:id",
+    passport.authenticate("jwt", {
+        session: false,
+    }),
+    matchGet
 );
 
 router.put(
-  "/:id",
-  [
-    passport.authenticate("jwt", {
-      session: false,
-    }),
-    // matchDuplicateCheckUpdate,
-  ],
-  matchUpdate
+    "/:id",
+    [
+        passport.authenticate("jwt", {
+            session: false,
+        }),
+        // matchDuplicateCheckUpdate,
+    ],
+    matchUpdate
 );
 
 router.delete(
-  "/:id",
-  passport.authenticate("jwt", {
-    session: false,
-  }),
-  matchDelete
+    "/:id",
+    passport.authenticate("jwt", {
+        session: false,
+    }),
+    matchDelete
 );
 
 module.exports = router;
